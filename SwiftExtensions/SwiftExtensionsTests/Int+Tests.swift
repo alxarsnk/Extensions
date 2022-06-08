5999485db8f93aef8d28914e4fc41bb42a938e99
Alexandr Arsenuk <alexandr.arsenuk@devexperts.com>
2022-06-08 17:56:02 +0300

    Add tests

---
+++ SwiftExtensions/SwiftExtensionsTests/Int+Tests.swift
@@ -0,0 +1,19 @@
+//
+//  Int+Tests.swift
+//  SwiftExtensionsTests
+//
+//  Created by Александр Арсенюк on 08.06.2022.
+//
+
+import Foundation
+import XCTest
+
+@testable import SwiftExtensions
+
+class IntExtensionsTets: XCTestCase {
+
+    func test001() {
+        XCTAssert(true)
+    }
+
+}
